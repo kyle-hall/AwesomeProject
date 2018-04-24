42e30b74d0bafe26decdcbc8f4222bb6bba656df
Kyle Hall <hallkd47@gmail.com>
2018-04-24 14:37:34 -0500

    adds Slider and Picker elements to Input screen.

--- src/Components/Input/Touchables.js
+++ src/Components/Input/Touchables.js
@@ -6,7 +6,10 @@
 import React from 'react'
 import {
   Alert,
+  Picker,
   Platform,
+  ScrollView,
+  Slider,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -17,6 +20,7 @@ import {
 } from 'react-native'
 
 type Props = {};
+type State = {language: string, sliderValue: number};
 
 const styles = StyleSheet.create({
   container: {
@@ -35,7 +39,8 @@ const styles = StyleSheet.create({
   }
 })
 
-export default class Touchables extends React.Component<Props> {
+export default class Touchables extends React.Component<Props, State> {
+  state = {language: 'java', sliderValue: 1}
 
   _onPressButton() {
     Alert.alert('You tapped the button')
@@ -66,7 +71,7 @@ export default class Touchables extends React.Component<Props> {
     )
 
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         <TouchableHighlight
           onPress={this._onPressButton}
           underlayColor="white"
@@ -81,7 +86,36 @@ export default class Touchables extends React.Component<Props> {
           </View>
         </TouchableOpacity>
         {touchableNativeFeedbackElement}
-      </View>
+        <Text>{this.state.language}</Text>
+        <Picker
+          style={{height: 150, width: 100}}
+          selectedValue={this.state.language}
+          onValueChange={(itemValue: string, itemIndex: number) => this.setState({language: itemValue})}
+        >
+          <Picker.Item label="Java" value="java"/>
+          <Picker.Item label="JavaScript" value="javascript" />
+          <Picker.Item label="Scala" value="scala" />
+          <Picker.Item label="Ruby" value="ruby" />
+          <Picker.Item label="Elixir" value="elixir" />
+          <Picker.Item label="Elm" value="elm" />
+          <Picker.Item label="Smalltalk" value="smalltalk" />
+        </Picker>
+        <View style={{marginTop: 50}}>
+          <Text>Slider value is: {this.state.sliderValue}</Text>
+          <Slider
+          style={{width: 100, marginTop: 50}}
+            minimumTrackTintColor='red'
+            maximumValue={10}
+            minimumValue={1}
+            value={this.state.sliderValue}
+            onValueChange={(itemValue: number, itemIndex: number) => {
+              this.setState({language: this.state.language, sliderValue: itemValue})
+            }}
+            step={1}
+          />
+          <View style={{height: 300}}></View>
+        </View>
+      </ScrollView>
     )
   }
 }
